246def42e0f373576e0b8f2f917dedb3c94ba96e
Karolina <dudzinnskakarolina@wp.pl>
2021-04-09 20:15:36 +0900

    fixes for map

--- obstacle-avoidance-turtlebot/src/masters_avodiance.py
+++ obstacle-avoidance-turtlebot/src/masters_avodiance.py
@@ -40,7 +40,7 @@ def callback1(dt):
     #print(dt.ranges , len(dt.ranges))
     closestObstacle = min(dt.ranges)
     closestObtacleAngle = dt.ranges.index(min(dt.ranges))
-    x = (383.5/2) + -(1) * ((383.5 - 0) / (10 - (-10)))
+    x = (407/2) + -(1) * ((407 - 0) / (10 - (-10)))
     #print(closestObstacle)
     #closestObstacle = (383.5/2) + -(closestObstacle) * ((383.5 - 0) / (10 - (-10)))
     #print(closestObstacle , closestObtacleAngle)
@@ -57,19 +57,24 @@ def callback(msg):
 	global J22
 
 
-	J11 = (383.5/2) + -(positionx) * ((383.5 - 0) / (10 - (-10)))
-	J11 = 383.5 - J11 + 7
+	J11 = (407/2) + -(positionx) * ((407 - 0) / (10 - (-10)))
+	J11 = 407 - J11 
 
-	J22 = (383.5/2) + -(positiony) * ((383.5 -  0 )/ (10 - (-10))) - 7
+	J22 = (407/2) + -(positiony) * ((407 -  0 )/ (10 - (-10))) 
 	#print(J11 , J22)
 	return J11 , J22 
 
 def cutCirle():
-	matrix = cv2.imread("/home/harumanager/map.pgm", cv2.IMREAD_COLOR)
-	print("zaczynam")
+	#matrix = cv2.imread("/home/harumanager/map.pgm", cv2.IMREAD_COLOR)
+	matrix = cv2.imread("/home/harumanager/catkin_ws/src/ROB10/maps/supermap1.pgm", cv2.IMREAD_COLOR)
+	print("zaczynam")	
 	pp.imshow(matrix)
+	pp.show()
+	matrix = matrix[200:607, 0:407]
+	pp.imshow(matrix)
+	pp.show()
 	#pp.close("all")
-	mask = sector_mask(matrix.shape,(J22,J11,),64.6,(0,360))
+	mask = sector_mask(matrix.shape,(J22,J11,),71.225,(0,360))
 	matrix[~mask] = 125
 	n = np.array(matrix)
 	number_of_white_pix = np.sum(matrix== 254)
@@ -80,10 +85,12 @@ def cutCirle():
 
 	print('Number of white pixels:', number_of_white_pix)
 	print('Number of black pixels:', number_of_black_pix)
-	matrix[194,160] = [254 , 50 ,50]
+	newCoorinatex = int(J22)
+	newCoorinatey = int(J11)
+	matrix[newCoorinatex,newCoorinatey] = [254 , 50 ,50]
 	matrix[199,151] = [254 , 50 ,50]
 	pp.imshow(matrix)
-	#pp.show()
+	pp.show()
 	#pp.close("all")
 	disntance_array = []
 	angle_array = []
@@ -93,8 +100,8 @@ def cutCirle():
 		#xcoords = (383.5/2) + -(xcoords) * ((383.5 - 0) / (10 - (-10)))
 		#xcoords = 383.5 - xcoords
 		#J22 = (383.5/2) + -(ycoords) * ((383.5 -  0 )/ (10 - (-10)))
-		#print(xcoords)
-		#print(ycoords)
+		print(xcoords)
+		print(ycoords)
 		#print (xcoords[3])
 		sizeArray = len(xcoords)
 		for x in range(sizeArray):
@@ -121,7 +128,7 @@ def cutCirle():
 			
 
 			distance =  math.sqrt(((J22 - ycoords[x])**2) + ((J11 - xcoords[x])**2))
-			distance = distance / 19.5			
+			distance = distance / 20.35			
 			dx =   J11
 			dy =   J22
 			p1 = (J11 , J22)
@@ -168,22 +175,24 @@ def cutCirle():
 		for xy in range(sizeArray):			
 			#print('value' , x)
 			##x_on_map - (x_on_map * 0.05) <= x <=  x_on_map + (x_on_map * 0.05)
-			if x - (x * 0.05) <= xcoords[xy] <= x + (x * 0.05):
+			if x - (x * 0.04) <= xcoords[xy] <= x + (x * 0.04):
 				indexxy = xy
-				print(indexxy , xcoords[xy] , x )
-				obstacleType = 1
-				if y - (y * 0.05 )<= ycoords[indexxy] <= y + (y * 0.05):
+				print(indexxy , xcoords[xy] , x )				
+				if y - (y * 0.04)<= ycoords[indexxy] <= y + (y * 0.04):
 					print("obstacle in both map and simulation" ,xcoords[xy] , x , ycoords[xy] , y  )
 					obstacleType = 1
-			
+					break
+			else:
+				print ("changing to 0")
+				obstacleType = 0 
 
 		#if(x_on_map + x_on_map * 0.005) >= x or x_on_map - (x_on_map* 0.005) <= x and y_on_map + (y_on_map * 0.005) >= y or y_on_map - (y_on_map * 0.005) <= y :
-		if x_on_map - (x_on_map * 0.05) <= x <=  x_on_map + (x_on_map * 0.05) and y_on_map - (y_on_map * 0.05) <= y <=  y_on_map + (y_on_map * 0.05) :
-			print("within a margin" ,x_on_map , x )
-			obstacleType = 1
-		else:
-			print("not within margin")
-			obstacleType = 0
+		#if x_on_map - (x_on_map * 0.05) <= x <=  x_on_map + (x_on_map * 0.05) and y_on_map - (y_on_map * 0.05) <= y <=  y_on_map + (y_on_map * 0.05) :
+			#print("within a margin" ,x_on_map , x )
+			#obstacleType = 1
+		#else:
+			#print("not within margin")
+			#obstacleType = 0
 	else:
 		if (math.isinf(closestObstacle) == True ):
 			print("There is no obstacles")
@@ -217,7 +226,8 @@ def point_on_circle():
     center = [J22, J11]
 
     angle = (math.radians(closestObtacleAngle - 90))
-    radius = closestObstacle * 19.5
+    radius = closestObstacle * 20.35
+    print("radius" ,radius)
     x = J11 + (radius * cos(angle))
     y = J22 + (radius * sin(angle))
     print("czy sie zgadzaja ",x,y)
@@ -226,12 +236,12 @@ def point_on_circle():
 
 def point_on_circle2():
 	center = [J22, J11]
-	for xy in range(365):
+	for xy in range(360):
 		angle = (math.radians(xy - 90))
-    	radius = closestObstacle * 19.5    	
-    	if(xy == closestObtacleAngle):
-    		x = J11 + (radius * cos(angle))
-    		y = J22 + (radius * sin(angle))	
+    	radius = closestObstacle * 71.2   	
+    	
+    	x = J11 + (radius * cos(angle))
+    	y = J22 + (radius * sin(angle))	
 	return x, y
 
 def sector_mask(shape,centre,radius,angle_range):
